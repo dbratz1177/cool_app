784668ba49af577295cf1f8a669111b9a94246a2
Dane Bratz <danebratz@gmail.com>
2014-09-15 14:16:35 -0700

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 CuntApp::Application.routes.draw do
-  get "foo_bars/boz"
-  get "foo_bars/jfl"
   get "static_pages/home"
   get "static_pages/help"
+  get "static_pages/about"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
